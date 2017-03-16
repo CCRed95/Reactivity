ed86095e4ff70011b8b6ef19cdfb4a72d4204971
CCRed95 <chipchipperson95@gmail.com>
2017-03-15 22:09:47 -0500

    initial commit

--- ReactivityDemo/MainWindow.xaml.cs
+++ ReactivityDemo/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Reactivity
+namespace ReactivityDemo
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
